0ad8a8d779fb619d137e1f923f8c910311851a0a
kronuc <denerator2@gmail.com>
2021-02-13 20:47:37 +0200

    debugging

--- TravelAgency.cpp
+++ TravelAgency.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include "Car.h"
+#include "Hotel.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	
+	Hotel hotel_0l;
+	hotel.setName("hotel 01");
+	cout<<hotel.getName()<<endl;
+	return 0;
 }
