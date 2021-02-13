f49f96b42cbd70900759cff2acbd6e837f5dd472
kronuc <denerator2@gmail.com>
2021-02-13 19:34:02 +0200

    wrote constructor

--- Hotel.cpp
+++ Hotel.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-Hotel()
+Hotel(int inhotelCode,std::string inName, std::string inCountry, std::string inCity, std::string inAddress, int inAmountOfStars, int inContactNumber)
 {
-	void setHotelCode(int hotelCode);
-	void setName(std::string name);
-	void setCountry(std::string country);
-	void setCity(std::string city);
-	void setAddress(std::string address);
-	void setAmountOfStars(int amountOfStars);
-	void setContactNumber(int contactNumber);
+	hotelCode = inHotelCode;
+	name = inName;
+	country = inCountry;
+	city = inCity;
+	address = inAddress;
+	amountOfStars = inAmountOfStars;
+	contactNumber = inContactNumber;	
 }
 
 ~Hotel();
